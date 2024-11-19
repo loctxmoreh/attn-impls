8f1c0d2bd2ce5afa890f7a11b626d9cc22e7481f
Tran Xuan Loc <loc.tran@moreh.com.vn>
2024-11-19 09:01:22 +0000

    milisec to sec

--- bench_sd3.py
+++ bench_sd3.py
@@ -56,7 +56,7 @@ def benchmark(name, func, batch_size, sequence_length, num_heads, head_dim, layo
 
     ms = triton.testing.do_bench(partial(func, query, key, value),
                                  warmup=WARM_UP, rep=REP, return_mode="mean")
-    tflops = calculate_tflops(ms * 10**3, batch_size, sequence_length, num_heads, head_dim)
+    tflops = calculate_tflops(ms * 1e-3, batch_size, sequence_length, num_heads, head_dim)
     print(f"{name:<20} \t{ms=} \t{tflops=}")
 
 
